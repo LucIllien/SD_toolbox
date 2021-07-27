18a380b53b9bf3a05b2e0cf8b0ee5fa6036f4561
luc-illien <lillien@gfz-potsdam.de>
2021-07-27 13:30:32 +0200

    initialise project

---
+++ measures.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numba import jit
+
+def integration_data(ydata, dt, event_indexes,y_bound,time_bound):
+    
+    return integrated_measure
+
+
+def derivative_data(ydata, dt, event_indexes)
+
+    return variation_measure
